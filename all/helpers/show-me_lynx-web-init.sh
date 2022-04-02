930f88c8a5830b3b00ff02b6248e5f2e088d093a
Fraig <craig@chrubuntu.craigbender.me>
2022-04-01 21:17:12 -0700

    Fixing issue from reducing cloud-init size down to < 16KB

--- all/helpers/show-me_lynx-web-init.sh
+++ all/helpers/show-me_lynx-web-init.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-export SHOW_ME_APP=${1,,}
-[[ -n $(command 2>/dev/null lynx) ]] || || { printf "Missing application \"lynx\".  Attempting to install\n";apt update && apt install -fqy lynx; }
-[[ -n $(command 2>/dev/null lynx) ]] || || { printf "Missing application \"lynx\".  Attempted installation failed.  Exiting\n";exit 1; }
+[[ $EUID -ne 0 ]] && { echo "This script must be run as root";exit 1; } || { true; }
+export SHOW_ME_APP=${1:-landscape}
+
+if [ -f /etc/lynx/lynx.cfg ];then sed -i -r "s/^#?(FORCE_SSL_COOKIE.*:|SET_COOKIE.*:|ACCEPT_ALL_COOKIE.*:)[^$]*/\1TRUE/;s/^#?(COOKIE_LOOSE_INVALID_DOMAINS:)[^$]*/\1$(hostname -I|sed -r 's/\x20/\n/g'|sed -r '/:|^$/d;1s/^/'$(hostname -d)'\n/g'|paste -sd,)/;s/^#?(FORCE_COOKIE_PROMPT.*:|FORCE_SSL_PROMPT.*:)[^$]*/\1yes/;s/^#?(COOKIE_.*FILE:.*$)/\1/" /etc/lynx/lynx.cfg;fi
+[[ -n $(command 2>/dev/null lynx) ]] || { printf "Missing application \"lynx\".  Attempting to install\n";apt update && apt install -fqy lynx; }
+[[ -f /etc/lynx/lynx.cfg ]] || { printf "Missing application parts of \"lynx\".  Attempting to install\n";apt update && apt install -fqy lynx; }
+[[ -n $(command 2>/dev/null lynx) || -f /etc/lynx/lynx.cfg ]] || { printf "Missing application \"lynx\".  Attempted installation failed.  Exiting\n";exit 1; }
 [[ -f /opt/show-me/all/helpers/${SHOW_ME_APP}.lynx ]] || { printf "No lynx script found for Show Me app \"${SHOW_ME_APP}\"";exit 0; }
+#comfigure lynx to auto accept cookes, ignore tls errors
+if [ -f /etc/lynx/lynx.cfg ];then sed -i -r "s/^#?(FORCE_SSL_COOKIE.*:|SET_COOKIE.*:|ACCEPT_ALL_COOKIE.*:)[^$]*/\1TRUE/;s/^#?(COOKIE_LOOSE_INVALID_DOMAINS:)[^$]*/\1$(hostname -I|sed -r 's/\x20/\n/g'|sed -r '/:|^$/d;1s/^/'$(hostname -d)'\n/g'|paste -sd,)/;s/^#?(FORCE_COOKIE_PROMPT.*:|FORCE_SSL_PROMPT.*:)[^$]*/\1yes/;s/^#?(COOKIE_.*FILE:.*$)/\1/" /etc/lynx/lynx.cfg;fi
+
 lynx -cmd_script=/opt/show-me/all/helpers/${SHOW_ME_APP}.lynx "https://${SHOW_ME_APP}.ubuntu-show.me"
+
 exit $?
 
