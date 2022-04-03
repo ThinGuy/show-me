10867947d84239ae1731cf7b51c9d51f30207e20
ThinGuy <19372989+ThinGuy@users.noreply.github.com>
2022-04-02 23:58:28 -0700

    fix petname dict paths

--- all/helpers/add-landscape-clients.sh
+++ all/helpers/add-landscape-clients.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # vim: set et ts=2 sw=2 filetype=bash :
-[[ ! -f /usr/local/lib/show-me/petname2 && -x /usr/local/bin/petname-helper.sh ]] && { mkdir -p /usr/local/share/petname2;/usr/local/bin/petname-helper.sh; }
+[[ ! -f /usr/local/lib/show-me/petname2 && -x /usr/local/bin/petname-helper.sh ]] && { mkdir -p /usr/local/lib/show-me/petname2;/usr/local/bin/petname-helper.sh; }
 [[ -f  /usr/local/lib/show-me/petname2/f-names.txt && -x /usr/local/bin/petname-helper.sh ]] && { true; } || { printf "\n\e[4G\e[0;1;38;2;255;0;0mError\x21\e[0m Issues with the petname-helper script, please report this problem.\e[0m\n";false;exit 1; }
 
 [[ -n ${1} && ${1,,} =~ -h ]] && { printf "${0##*/} [# of instances per release (ipr)]\n\nDefault ipr = 2\n\n";exit 2; }
@@ -15,9 +15,9 @@ for X in $(lxc image list -cfl|awk '/^\|/&&!/FIN/{print $2":"$4}');do
   [[ ${A} =~ f ]] && export R=Focal;
   [[ ${A} =~ i ]] && export R=Impish;
   [[ ${A} =~ j ]] && export R=Jammy;
-declare -ag NAMES=($(cat /usr/local/share/petname2/${A:0:1}-names.txt))
-declare -ag ADVERBS=($(cat /usr/local/share/petname2/${A:0:1}-adverbs.txt))
-declare -ag ADJECTIVES=($(cat /usr/local/share/petname2/${A:0:1}-adjectives.txt))
+declare -ag NAMES=($(cat /usr/local/lib/show-me/petname2/${A:0:1}-names.txt))
+declare -ag ADVERBS=($(cat /usr/local/lib/show-me/petname2/${A:0:1}-adverbs.txt))
+declare -ag ADJECTIVES=($(cat /usr/local/lib/show-me/petname2/${A:0:1}-adjectives.txt))
   for Y in $(seq 1 1 $MAXI);do
     [[ ${Y} = 1 ]] && export W=st;
     [[ ${Y} = 2 ]] && export W=nd;
