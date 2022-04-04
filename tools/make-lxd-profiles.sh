07be06a6465c7201825f41a78aaa023b8da456b8
ThinGuy <19372989+ThinGuy@users.noreply.github.com>
2022-04-04 13:38:58 -0500

    tests -> tools

---
+++ tools/make-lxd-profiles.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+export PROG_DIR="$( cd $(dirname ${0})/.. && pwd )"
+
+if [ ! $(lxc &>/dev/null profile device get default eth0 network;echo $?) -o ! $(lxc &>/dev/null profile device get default root pool;echo $?) ];then printf 1>&2 "\n\nLXD is not configured\x21  Please run lxd init\n\n";exit 1;fi
+
+export PROG_DIR="$( cd $(dirname ${0})/.. && pwd )"
+export SHOW_ME_ARCH=$(dpkg --print-architecture)
+export LXD_DISK_POOL=default
+export LXD_NIC_PARENT_0=lxdbr0
+export LXD_NIC_PARENT_1=br-bond0
+export LXD_DISK_SIZE_MAAS=50
+export LXD_DISK_SIZE_LAND=20
+for A in maas landscape;do 
+  for P in aws multipass;do
+    lxc 2>/dev/null profile create "${A}-on-${P}"
+    [[ $P = aws ]] && { declare -ag NICS=(ens5); }
+    [[ $P = multipass ]] { && declare -ag NICS=(enp0s3 enp0s8); }
+    [[ $A = maas ]] && { export LXD_DISK_SIZE=${LXD_DISK_SIZE_MAAS}; }
+    [[ $A = landscape ]] && { export LXD_DISK_SIZE=${LXD_DISK_SIZE_LAND}; }
+cat <<-EOF|sed -r 's/[ \t]+$//g'|sed -r '/^$/d'|tee 1>/dev/null lxd-profile_${A}-on-${P}_${SHOW_ME_ARCH}.yaml
+config:
+  boot.autostart: "false"
+  migration.incremental.memory: "false"
+  security.nesting: "true"
+  user.network-config: |
+    version: 2
+    ethernets:
+      ens5:
+        dhcp4: true
+        dhcp6: false
+        accept-ra: false
+        optional: no
+  user.user-data: |
+$(cat <<-'YAML'|sed -r 's/^/    /g;s/[ \t]+$//g' < ${PROG_DIR}/${A}/${P}/${P}_show-me_${A}_user-data_${SHOW_ME_ARCH}.yaml
+YAML
+)
+description: 'Simulate running ${A} on ${P}'
+devices:
+$(if [[ ${#NICS[@]} -eq 2 ]];then
+cat <<-NET2
+ ${NICS[0]}:
+    name: ${NICS[0]}
+    nictype: bridged
+    parent: ${LXD_NIC_PARENT_0}
+    type: nic
+ ${NICS[1]}:
+    name: ${NICS[1]}
+    nictype: bridged
+    parent: ${LXD_NIC_PARENT_1}
+    type: nic
+NET2
+elif [[ ${#NICS[@]} -eq 1 ]];then
+cat <<-NET1
+ ${NICS[0]}:
+    name: ${NICS[0]}
+    nictype: bridged
+    parent: ${LXD_NIC_PARENT_0}
+    type: nic
+NET1
+fi)
+  root:
+    path: /
+    pool: ${LXD_DISK_POOL}
+    size: ${LXD_DISK_SIZE}GB
+    type: disk
+name: ${A}-on-${P}
+EOF
+done
+done
