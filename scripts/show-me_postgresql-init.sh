c78334135f15f2acded1ba05e6475f8184fe90de
ThinGuy <19372989+ThinGuy@users.noreply.github.com>
2022-04-10 13:41:07 -0700

    changing from quickstart to manual

--- scripts/show-me_postgresql-init.sh
+++ scripts/show-me_postgresql-init.sh
@@ -19,13 +19,13 @@ export PG_DBHBA="/etc/postgresql/${PG_DBVER}/main/pg_hba.conf"
 export PG_DBHOST='maas.ubuntu-show.me'
 export PG_DBUSER=postgres
 export PG_DBNAME=postgres PG_DBPORT=5432
-export PG_DBSSL_CRT=/etc/ssl/certs/show-me_ca.crt
-export PG_DBSSL_PEM=/etc/ssl/certs/show-me_host.crt
-export PG_DBSSL_KEY=/etc/ssl/private/show-me_host.key
-su - postgres -c "psql postgres -c \"ALTER SYSTEM SET ssl to 'on';\""
-su - postgres -c "psql postgres -c \"ALTER SYSTEM SET ssl_ca_file to '${PG_DBSSL_CRT}';\""
-su - postgres -c "psql postgres -c \"ALTER SYSTEM SET ssl_cert_file to '${PG_DBSSL_PEM}';\""
-su - postgres -c "psql postgres -c \"ALTER SYSTEM SET ssl_key_file to '${PG_DBSSL_KEY}';\""
+#export PG_DBSSL_CRT=/etc/ssl/certs/show-me_ca.crt
+#export PG_DBSSL_PEM=/etc/ssl/certs/show-me_host.crt
+#export PG_DBSSL_KEY=/etc/ssl/private/show-me_host.key
+#su - postgres -c "psql postgres -c \"ALTER SYSTEM SET ssl to 'on';\""
+#su - postgres -c "psql postgres -c \"ALTER SYSTEM SET ssl_ca_file to '${PG_DBSSL_CRT}';\""
+#su - postgres -c "psql postgres -c \"ALTER SYSTEM SET ssl_cert_file to '${PG_DBSSL_PEM}';\""
+#su - postgres -c "psql postgres -c \"ALTER SYSTEM SET ssl_key_file to '${PG_DBSSL_KEY}';\""
 su - postgres -c "psql postgres -c \"ALTER SYSTEM SET listen_addresses to '*';\""
 su - postgres -c "psql postgres -c \"ALTER SYSTEM SET max_connections to '500';\""
 su - postgres -c "psql postgres -c \"ALTER SYSTEM SET max_prepared_transactions to '500';\""
@@ -52,7 +52,7 @@ SQL
 echo "${SSP_DBHOST}:${SSP_DBPORT}:${SSP_DBNAME}:${SSP_DBUSER}:${SSP_DBPASS}"|su postgres -c 'tee 1>/dev/null /var/lib/postgresql/.pgpass.ssp'
 chmod 0600 /var/lib/postgresql/.pgpass.ssp
 chown postgres:postgres /var/lib/postgresql/.pgpass.ssp   
-printf '%-08s%-016s%-016s%-024s%s\n' hostssl ${SSP_DBNAME} ${SSP_DBUSER} '::/0' md5 hostssl ${SSP_DBNAME} ${SSP_DBUSER} '0.0.0.0/0' md5|su - postgres -c 'tee -a '${SSP_DBHBA}''
+printf '%-08s%-016s%-016s%-024s%s\n' host ${SSP_DBNAME} ${SSP_DBUSER} '::/0' md5 host ${SSP_DBNAME} ${SSP_DBUSER} '0.0.0.0/0' md5|su - postgres -c 'tee -a '${SSP_DBHBA}''
 su - postgres -c 'psql postgres -c "SELECT pg_reload_conf();"'
 
 # PostgreSQL setup for Canonical Candid
@@ -69,14 +69,14 @@ su - postgres -c 'psql -c "CREATE DATABASE '${CANDID_DBNAME}' WITH OWNER '"'"''$
 echo "${CANDID_DBHOST}:${CANDID_DBPORT}:${CANDID_DBNAME}:${CANDID_DBUSER}:${CANDID_DBPASS}"|su postgres -c 'tee 1>/dev/null /var/lib/postgresql/.pgpass.candid'
 chmod 0600 /var/lib/postgresql/.pgpass.candid
 chown postgres:postgres /var/lib/postgresql/.pgpass.candid 
-printf '%-08s%-016s%-016s%-024s%s\n' hostssl ${CANDID_DBNAME} ${CANDID_DBUSER} '::/0' md5 hostssl ${CANDID_DBNAME} ${CANDID_DBUSER} '0.0.0.0/0' md5|su - postgres -c 'tee -a '${CANDID_DBHBA}''
+printf '%-08s%-016s%-016s%-024s%s\n' host ${CANDID_DBNAME} ${CANDID_DBUSER} '::/0' md5 host ${CANDID_DBNAME} ${CANDID_DBUSER} '0.0.0.0/0' md5|su - postgres -c 'tee -a '${CANDID_DBHBA}''
 su - postgres -c 'psql postgres -c "SELECT pg_reload_conf();"'
 
 # PostgreSQL setup for Landscape
 export LANDSCAPE_DBVER=$(psql -V|awk '{gsub(/\..*$/,"");print $3}')
 export LANDSCAPE_DBHBA="/etc/postgresql/${LANDSCAPE_DBVER}/main/pg_hba.conf"
 export LANDSCAPE_DBHOST='maas.ubuntu-show.me'
-export LANDSCAPE_DBUSER=landscape
+export LANDSCAPE_DBUSER=landscape_superuser
 export LANDSCAPE_DBNAME='landscapedb'
 export LANDSCAPE_DBPORT=5432
 export LANDSCAPE_DBPASS="$(env LANG=C LC_ALL=C tr 2>/dev/null -dc "[:alnum:]" < /dev/urandom|fold -w12|head -n1)"
@@ -86,7 +86,7 @@ su - postgres -c 'psql -c "CREATE DATABASE '${LANDSCAPE_DBNAME}' WITH OWNER '"'"
 echo "${LANDSCAPE_DBHOST}:${LANDSCAPE_DBPORT}:${LANDSCAPE_DBNAME}:${LANDSCAPE_DBUSER}:${LANDSCAPE_DBPASS}"|su postgres -c 'tee 1>/dev/null /var/lib/postgresql/.pgpass.landscape'
 chmod 0600 /var/lib/postgresql/.pgpass.landscape
 chown postgres:postgres /var/lib/postgresql/.pgpass.landscape
-printf '%-08s%-016s%-016s%-024s%s\n' hostssl ${LANDSCAPE_DBNAME} ${LANDSCAPE_DBUSER} '::/0' md5 hostssl ${LANDSCAPE_DBNAME} ${LANDSCAPE_DBUSER} '0.0.0.0/0' md5|su - postgres -c 'tee -a '${LANDSCAPE_DBHBA}'' 
+printf '%-08s%-08s%-060s%-020s%s\n' host all landscape,landscape_maintenance,landscape_superuser '::/0' md5 host all landscape,landscape_maintenance,landscape_superuser '0.0.0.0/0' md5|su - postgres -c 'tee -a '${LANDSCAPE_DBHBA}''
 su - postgres -c 'psql postgres -c "SELECT pg_reload_conf();"'
 
 # PostgreSQL setup for Canonical RBAC
@@ -101,7 +101,7 @@ export RBAC_DBCON="postgres://${RBAC_DBUSER}:${RBAC_DBPASS}@${RBAC_DBHOST}:${RBA
 su - postgres -c 'psql -c "CREATE ROLE '${RBAC_DBUSER}' WITH SUPERUSER CREATEDB CREATEROLE LOGIN REPLICATION ENCRYPTED PASSWORD '"'"''${RBAC_DBPASS}''"'"';"'
 su - postgres -c 'psql -c "CREATE DATABASE '${RBAC_DBNAME}' WITH OWNER '"'"''${RBAC_DBUSER}''"'"';"'
 echo "${RBAC_DBHOST}:${RBAC_DBPORT}:${RBAC_DBNAME}:${RBAC_DBUSER}:${RBAC_DBPASS}"|su postgres -c 'tee 1>/dev/null /var/lib/postgresql/.pgpass.rbac'
-printf '%-08s%-016s%-016s%-024s%s\n' hostssl ${RBAC_DBNAME} ${RBAC_DBUSER} '::/0' md5 hostssl ${RBAC_DBNAME} ${RBAC_DBUSER} '0.0.0.0/0' md5|su - postgres -c 'tee -a '${RBAC_DBHBA}''
+printf '%-08s%-016s%-016s%-024s%s\n' host ${RBAC_DBNAME} ${RBAC_DBUSER} '::/0' md5 host ${RBAC_DBNAME} ${RBAC_DBUSER} '0.0.0.0/0' md5|su - postgres -c 'tee -a '${RBAC_DBHBA}''
 su - postgres -c 'psql postgres -c "SELECT pg_reload_conf();"'
 
 # PostgreSQL setup for Canonical MAAS
@@ -124,7 +124,7 @@ su - postgres -c 'psql -c "CREATE ROLE '${MAAS_DBUSER}' WITH SUPERUSER CREATEDB 
 su - postgres -c 'psql -c "CREATE DATABASE '${MAAS_DBNAME}' WITH OWNER '"'"''${MAAS_DBUSER}''"'"';"'
 echo "${MAAS_DBHOST}:${MAAS_DBPORT}:${MAAS_DBNAME}:${MAAS_DBUSER}:${MAAS_DBPASS}"|su postgres -c 'tee 1>/dev/null /var/lib/postgresql/.pgpass.maas'
 cp -a /var/lib/postgresql/.pgpass* /home/$(id -un 1000)/;chown -R "$(id -un 1000):$(id -un 1000)" /home/$(id -un 1000)/\.*
-printf '%-08s%-016s%-016s%-024s%s\n' hostssl ${MAAS_DBNAME} ${MAAS_DBUSER} '::/0' md5 hostssl ${MAAS_DBNAME} ${MAAS_DBUSER} '0.0.0.0/0' md5|su - postgres -c 'tee -a '${MAAS_DBHBA}''
+printf '%-08s%-016s%-016s%-024s%s\n' host ${MAAS_DBNAME} ${MAAS_DBUSER} '::/0' md5 host ${MAAS_DBNAME} ${MAAS_DBUSER} '0.0.0.0/0' md5|su - postgres -c 'tee -a '${MAAS_DBHBA}''
 su - postgres -c 'psql postgres -c "SELECT pg_reload_conf();"'
 for P in MAAS SSP RBAC CANDID LANDSCAPE PG;do set|grep "^${P}_";done|sort -uV|tee ~/.show-me.rc|su - $(id -un 1000) -c 'tee ~/.show-me.rc'|su - postgres -c 'tee ~/.show-me.rc'
 echo 'for RC in $(find ~/ -maxdepth 1 -type f -iname ".show-me*.rc");do source $RC;done'|tee -a /root/.bashrc |su - $(id -un 1000) -c 'tee -a ~/.bashrc'|su - postgres -c 'tee -a ~/.bashrc'
