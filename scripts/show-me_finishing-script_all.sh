311e0a29bfd88554802cd81a417b1c457bac7694
craig bender <19372989+ThinGuy@users.noreply.github.com>
2022-04-11 09:38:42 -0700

    Update finishing to always update show-me

--- scripts/show-me_finishing-script_all.sh
+++ scripts/show-me_finishing-script_all.sh
@@ -2,8 +2,10 @@
 # vim: set et ts=2 sw=2 filetype=bash :
 
 { [[ $CLOUD_DEBUG ]] &>/dev/null; } && { { set -x; } &>/dev/null; }
+
 [[ $EUID -ne 0 ]] && { echo "${0##*/} must be run as root or via sudo";exit 1; } || { true; }
 
+cd /opt/show-me;git pull
 
 
 { [[ $CLOUD_DEBUG ]] &>/dev/null; } && { { set +x; } &>/dev/null; }
